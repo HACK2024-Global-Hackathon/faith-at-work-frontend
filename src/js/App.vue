d3fb33a3415604aa9b8840c969295763e44e3201
John Yong <whipermr5@gmail.com>
2024-11-02 11:20:35 +0800

    Add share and sign up icons

--- src/js/App.vue
+++ src/js/App.vue
@@ -315,8 +315,23 @@ export default {
                 <p class="card-text text-body-secondary my-auto">{{ dayjs(event.datetime_start).calendar(null, { sameElse: 'ddd, DD MMM [at] LT' }) }}</p>
               </div>
               <div class="d-flex justify-content-end mt-3">
-                <div class="pe-1"><button class="btn btn-primary" @click="shareUrl(event.eventbrite_url || DEFAULT_URL)">Share</button></div>
-                <div><a class="btn btn-primary" :href="event.eventbrite_url || DEFAULT_URL">Sign up</a></div>
+                <div class="pe-1">
+                  <button class="btn btn-primary" @click="shareUrl(event.eventbrite_url || DEFAULT_URL)">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                      <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3"/>
+                    </svg>
+                    Share
+                  </button>
+                </div>
+                <div>
+                  <a class="btn btn-primary" :href="event.eventbrite_url || DEFAULT_URL">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                      <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                      <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                    </svg>
+                    Sign up
+                  </a>
+                </div>
               </div>
             </div>
           </div>
